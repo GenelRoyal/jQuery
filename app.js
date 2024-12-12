9251bf309b96bbfabc91cd9867f2de991bcab064
Masahiro Yamashita <milky.tafl.0225@gmail.com>
2024-12-12 05:39:01 +0000

    jQuery part2

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// $(function(){
+//   $('.box1').slideUp(1000);
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+// $(function(){
+//   $('.box1').hide();
+// });
+
+$(function(){
+  $('.box1').slideDown(1000,function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
